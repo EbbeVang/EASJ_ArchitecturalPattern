96be7b8cd0c68916fc364ab1d0957f4d1fa091bb
ev <ev@evang.dk>
2014-08-13 14:30:13 +0200

    hfhfh

---
+++ WindowsStoreApp/ViewModel/TapasViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsStoreApp.Annotations;
+using WindowsStoreApp.TapasServiceReference;
+
+namespace WindowsStoreApp.ViewModel
+{
+    class TapasViewModel: INotifyPropertyChanged
+    {
+        private ObservableCollection<Tapas> _tapasViewListe;
+
+        public ObservableCollection<Tapas> TapasViewListe
+        {
+            get { return _tapasViewListe; }
+            set
+            {
+                _tapasViewListe = value;
+                OnPropertyChanged("TapasViewListe");
+            }
+        }
+
+        public TapasViewModel()
+        {
+            TapasServiceClient tapasDataClient = new TapasServiceClient();
+
+            Task<ObservableCollection<Tapas>> allTapasAsync = tapasDataClient.GetAllTapasAsync();
+            allTapasAsync.Wait();
+
+            _tapasViewListe = new ObservableCollection<Tapas>(allTapasAsync.Result);
+        }
+
+
+
+
+        #region Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
