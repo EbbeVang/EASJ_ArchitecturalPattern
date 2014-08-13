14bd39b12116ef266b71782a2ab94535b503a935
ev <ev@evang.dk>
2014-08-13 14:29:52 +0200

    a

---
+++ Persitence/DBPersistence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelClassLibrary;
+
+namespace Persitence
+{
+    public class DBPersistence : ITapasService
+    {
+        private SqlConnection _connection;
+        private readonly static DBPersistence _instance = new DBPersistence();
+
+        public static DBPersistence Instance
+        {
+            get { return _instance; }
+        }
+
+        private DBPersistence()
+        {
+            String connectionString = @"Data Source=(localdb)\Projects;Initial Catalog=TapasDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+        }
+
+
+        public List<Tapas> GetAllTapas()
+        {
+            String sql = "select * from Tapas";
+            SqlCommand sqlCommand = new SqlCommand(sql,_connection);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            List<Tapas> tapasList = new List<Tapas>();
+            while (reader.Read())
+            {
+                Tapas tapas = new Tapas();
+                tapas.TapasNo = reader.GetInt32(0);
+                tapas.Name = reader.GetString(1);
+                tapas.Price = Convert.ToDouble(reader.GetDecimal(2));
+
+                tapasList.Add(tapas);
+            }
+
+            reader.Close();
+            return tapasList;
+
+        }
+    }
+}
---
+++ Persitence/TapasService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ModelClassLibrary;
+
+namespace Persitence
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TapasService" in both code and config file together.
+    public class TapasService : ITapasService
+    {
+        public List<Tapas> GetAllTapas()
+        {
+            return DBPersistence.Instance.GetAllTapas();
+        }
+    }
+}
