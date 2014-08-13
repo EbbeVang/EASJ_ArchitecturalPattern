e42133158fc635eb5a40e35e6777d7b751d67100
Mohammad Homayoon Fayez <mhfayez@hotmail.com>
2014-08-13 10:53:35 +0200

    Adds Guest class

---
+++ ModelClassLibrary/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelClassLibrary
+{
+    public class Guest
+    {
+        private string name;
+        public string Name {get; set;}
+
+        private string phone;
+        public string Phone {get; set;}
+    }
+}
